06a4eec63115085af59e602b034d072c99293b08
Girlspower <tapati_us@yahoo.com>
2020-10-17 01:00:54 -0500

    first commit

--- target/site/cucumber-pretty/report.js
+++ target/site/cucumber-pretty/report.js
@@ -1,62 +1,62 @@
 $(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("framework.feature");
 formatter.feature({
   "line": 1,
-  "name": "Search Functionality",
+  "name": "Creat An Account",
   "description": "",
-  "id": "search-functionality",
+  "id": "creat-an-account",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 4,
-  "name": "Veryfy search field",
+  "line": 3,
+  "name": "Veryfy User can create a new account",
   "description": "",
-  "id": "search-functionality;veryfy-search-field",
+  "id": "creat-an-account;veryfy-user-can-create-a-new-account",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
     {
-      "line": 3,
+      "line": 2,
       "name": "@Sanity"
     }
   ]
 });
 formatter.step({
-  "line": 5,
+  "line": 4,
   "name": "Launch \"\u003cURL\u003e\"",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 6,
+  "line": 5,
   "name": "click on Sign In",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 7,
+  "line": 6,
   "name": "I can nevigate to the Creat an account page",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 8,
+  "line": 7,
   "name": "I Expect to Write First Name \"\u003cFirstName\u003e\"",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 9,
+  "line": 8,
   "name": "I Expect to Write Last Name \"\u003cLastName\u003e\"",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 10,
+  "line": 9,
   "name": "I Expect to Write Email Address \"\u003cEmailAddress\u003e\"",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 11,
+  "line": 10,
   "name": "I can create a \"\u003cPassword\u003e\"",
   "keyword": "And "
 });
 formatter.step({
-  "line": 12,
+  "line": 11,
   "name": "Verify I can see Show Password button",
   "keyword": "And "
 });
@@ -70,22 +70,21 @@ formatter.match({
   "location": "Stepdep1Page.launch(String)"
 });
 formatter.result({
-  "duration": 2937607706,
-  "status": "passed"
+  "duration": 268046052,
+  "error_message": "java.lang.NullPointerException: null value in entry: url\u003dnull\r\n\tat com.google.common.collect.CollectPreconditions.checkEntryNotNull(CollectPreconditions.java:32)\r\n\tat com.google.common.collect.SingletonImmutableBiMap.\u003cinit\u003e(SingletonImmutableBiMap.java:42)\r\n\tat com.google.common.collect.ImmutableBiMap.of(ImmutableBiMap.java:72)\r\n\tat com.google.common.collect.ImmutableMap.of(ImmutableMap.java:124)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:276)\r\n\tat com.testUtill1Page.TbasePage.getURL(TbasePage.java:47)\r\n\tat Stepdep1Page.Stepdep1Page.launch(Stepdep1Page.java:17)\r\n\tat ✽.Given Launch \"\u003cURL\u003e\"(framework.feature:4)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "Stepdep1Page.click_on_Sign_In()"
 });
 formatter.result({
-  "duration": 341127629,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Stepdep1Page.i_can_nevigate_to_the_Creat_an_account_page()"
 });
 formatter.result({
-  "duration": 4949256806,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -97,8 +96,7 @@ formatter.match({
   "location": "Stepdep1Page.i_Expect_to_Write_First_Name(String)"
 });
 formatter.result({
-  "duration": 448091040,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -110,8 +108,7 @@ formatter.match({
   "location": "Stepdep1Page.i_Expect_to_Write_Last_Name(String)"
 });
 formatter.result({
-  "duration": 178958139,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -123,8 +120,7 @@ formatter.match({
   "location": "Stepdep1Page.i_Expect_to_Write_Email_Address(String)"
 });
 formatter.result({
-  "duration": 303830837,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -136,14 +132,12 @@ formatter.match({
   "location": "Stepdep1Page.i_can_create_a(String)"
 });
 formatter.result({
-  "duration": 227411719,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Stepdep1Page.verify_I_can_see_Show_Password_button()"
 });
 formatter.result({
-  "duration": 97875531,
-  "status": "passed"
+  "status": "skipped"
 });
 });
